94e33b5b777ce75e8c676b80508e465c3d28c289
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-09-10 03:32:29 +0800

    Add some C++ snippets.

--- Mile.SelfContainedUniversalWindowsPlatformApp/MainPage.cpp
+++ Mile.SelfContainedUniversalWindowsPlatformApp/MainPage.cpp
@@ -2,6 +2,27 @@
 #include "MainPage.h"
 #include "MainPage.g.cpp"
 
+// #include "D:\Projects\MouriNaruto\NanaRun\ThirdParty\Detours\detours.h"
+// #pragma comment(lib, "D:\\Projects\\MouriNaruto\\NanaRun\\Output\\Binaries\\Release\\x64\\Detours.lib")
+
+// static LONG dwSlept = 0;
+
+// // Target pointer for the uninstrumented Sleep API.
+// //
+// static VOID(WINAPI* TrueSleep)(DWORD dwMilliseconds) = Sleep;
+
+// // Detour function that replaces the Sleep API.
+// //
+// VOID WINAPI TimedSleep(DWORD dwMilliseconds)
+// {
+//     // Save the before and after times around calling the Sleep API.
+//     DWORD dwBeg = GetTickCount();
+//     TrueSleep(dwMilliseconds);
+//     DWORD dwEnd = GetTickCount();
+
+//     InterlockedExchangeAdd(&dwSlept, dwEnd - dwBeg);
+// }
+
 using namespace winrt;
 using namespace Windows::UI::Xaml;
 
@@ -9,6 +30,13 @@ namespace winrt::MileSelfContainedUniversalWindowsPlatformApp::implementation
 {
     MainPage::MainPage()
     {
+        // DetourTransactionBegin();
+        // DetourUpdateThread(GetCurrentThread());
+        // DetourAttach(&(PVOID&)TrueSleep, TimedSleep);
+        // DetourTransactionCommit();
+
+        // ::Sleep(1000);
+
         InitializeComponent();
     }
 
@@ -24,6 +52,6 @@ namespace winrt::MileSelfContainedUniversalWindowsPlatformApp::implementation
 
     void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
     {
-        myButton().Content(box_value(L"Clicked"));
+        //myButton().Content(box_value(L"Clicked"));
     }
 }
